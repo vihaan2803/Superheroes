bbdb5a4f821a1279b5d3c79f9afb1e66487d98cd
vihaan2803 <74404558+vihaan2803@users.noreply.github.com>
2021-02-02 14:14:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+var canvas= new fabric.Canvas("myCanvas");
+superhero_image_width= 30;
+superhero_image_height= 30;
+player_x= 10;
+player_y= 10;
+var player_object="";
+function player_updat(){
+    fabric.Image.fromURL("https://i.postimg.cc/zDwfFdYY/player.png", 
+    function(Img) {
+         player_object = Img;
+          player_object.scaleToWidth(150); 
+          player_object.scaleToHeight(140);
+         player_object.set({ top:player_y, left:player_x });
+          canvas.add(player_object); });
+}
+
+function new_image(get_image) { 
+    fabric.Image.fromURL(get_image,
+         function(Img) { 
+             block_image_object = Img;
+              block_image_object.scaleToWidth(superhero_image_width); 
+              block_image_object.scaleToHeight(superhero_image_height); 
+    block_image_object.set({ top:player_y, left:player_x }); 
+    canvas.add(block_image_object); 
+}
+)
+;
+ }
